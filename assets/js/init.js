5d66044b73259b43ae763e92e0d027d497557622
cmarkham7272@gmail.com <cmarkham7272@gmail.com>
2023-04-07 00:27:33 -0500

    added card classes and modals for each project.

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,10 +1,10 @@
 
 //Getting Started Button Leads the User to the Getting Started Page
-var startingButton = document.getElementById("getting-started-btn") 
-startingButton.addEventListener("click", switchToForm)
+var startingButton = document.getElementById("getting-started-btn");
+startingButton.addEventListener("click", switchToForm);
 
 function switchToForm() {
-  window.location.href = ("./form.html")
+  window.location.href = ("./form.html");
 }
 
 //Log-In button click leads the user to the Log-In Page
@@ -12,5 +12,10 @@ var logInButton = document.getElementById("log-in-btn")
 logInButton.addEventListener("click", switchToLogIn)
 
 function switchToLogIn() {
-  location.assign("./logIn.html")
+  location.assign("./logIn.html");
 }
+
+
+
+
+
---
+++ assets/js/modal.js
@@ -0,0 +1,89 @@
+// Added JS for Modal (delete if this isn't what group agrees on.)
+
+// Modal 1
+var modalButton1 = document.getElementById("modal-btn1");
+var span = document.getElementsByClassName("close")[0];
+
+
+function openModal1() {
+
+  var modal = document.getElementById("modal-project-1");
+
+  function openModal() {
+    modal.style.display = "block";
+  }
+
+  openModal()
+  
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
+
+modalButton1.addEventListener("click", openModal1)
+
+// Modal 2
+
+var modalButton2 = document.getElementById("modal-btn2");
+
+
+function openModal2() {
+
+  var modal = document.getElementById("modal-project-2");
+  var span2 = document.getElementsByClassName("close")[0];
+
+  function openModal() {
+    modal.style.display = "block";
+  }
+
+  openModal()
+  
+  
+  span2.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
+
+modalButton2.addEventListener("click", openModal2)
+
+// Modal 3
+
+
+// var modalButton3 = document.getElementById("modal-btn3");
+
+
+// function openModal3() {
+
+//   var modal = document.getElementById("modal-project-2");
+//   var span = document.getElementsByClassName("close")[0];
+
+//   function openModal() {
+//     modal.style.display = "block";
+//   }
+
+//   openModal3()
+  
+//   span.onclick = function() {
+//     modal.style.display = "none";
+//   }
+  
+//   window.onclick = function(event) {
+//     if (event.target == modal) {
+//       modal.style.display = "none";
+//     }
+//   }
+// }
+
+// modalButton3.addEventListener("click", openModal3)
