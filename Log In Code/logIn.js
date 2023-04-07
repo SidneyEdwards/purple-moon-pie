1a6a5191f995dcbd33434c72c52d5114e7e42509
cmarkham7272@gmail.com <cmarkham7272@gmail.com>
2023-04-06 19:17:21 -0500

    fixed button errors from merge conflict.

--- Log In Code/logIn.js
+++ Log In Code/logIn.js
@@ -1,4 +1,14 @@
-var form = document.getElementById("login-div")
+
+//If the User does not have a log-in, they can click the "New to QRYou button" to be re-directed to the sign-up form page
+var form =document.getElementById("login-div")
+
+var NewToQRYou = document.getElementById("sign-up") 
+NewToQRYou.addEventListener("click", switchToForm2)
+
+function switchToForm2() {
+window.location.href = ("form.html")
+};
+
 
 
 //Saves the user name and password for the user in local storage
@@ -8,14 +18,29 @@ if(localStorage.getItem("username") && localStorage.getItem("password")){
 
 }
 
-form.addEventListener("submit", (e) => {
-    event.preventDefault()
-var username = document.getElementById("username").value;
-var password = document.getElementById ("password").value;
+form.addEventListener("submit", (i) => {
+    i.preventDefault()
+var username= document.getElementById("username").value;
+var password=document.getElementById ("password").value;
 
 
 
 //saves the username and password to local storage
 localStorage.setItem("username", username)
 localStorage.setItem("password", password)
+
 });
+
+//Checks that the Username and Password are correct/match
+
+
+
+//Clicking the log-in button re-directs the user to their Homepage
+var userLogInButton = document.getElementById("btn btn-info") 
+userLogInButton.addEventListener("click", switchToForm)
+
+function switchToForm() {
+  window.location.href = ("/QR Create Code/QRCreate.html")
+}
+
+
