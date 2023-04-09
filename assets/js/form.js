148daac13ca0cc54f5e86338d549f0c4020d7ade
cmarkham7272@gmail.com <cmarkham7272@gmail.com>
2023-04-08 22:32:25 -0500

    got rid of pointless code and changed buttons on form.html/js for local storage

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,31 +1,6 @@
-var linkFormEl = $('#add-links-form');
 var linkListEl = $('#added-links-list');
-var fetchButton = document.querySelector('#go-to-user-homepage')
 var requestLinkedInURL = ""
 var localStorageBtn = document.getElementById("submit-local-storage-btn")
-
-// create function to handle form submission
-function submitLink(event) {
-  event.preventDefault()
-  // select form element by its `name` attribute and get its value
-  var linkItem = $('input[name="link-input"]').val();
-
-  // if there's nothing in the form entered, don't print to the page
-  if (!linkItem) {
-
-    return;
-  }
-  // print to the page
-  linkListEl.append('<a href="' + linkItem + '">' + linkItem + '</a>' + ", " );
-
-
-  // clear the form input element
-  $('input[name="link-input"]').val('');
-}
-// Create a submit event listener on the form element
-linkFormEl.on('submit', submitLink);
-
-
 
 function getApiGithub(event) {
   event.preventDefault();
@@ -45,12 +20,13 @@ function getApiGithub(event) {
 
     });
 }
+
+// getApiGithub()
+
 function getApiLinkedIn(event) {
   event.preventDefault();
   
   var LinkedInUsername = document.getElementById('LinkedIn-username').value;
-
-  console.log(githubUsername);
 
   var requestUrl = 'https://api.github.com/users/' + LinkedInUsername + '/repos';
   
@@ -76,31 +52,23 @@ function switchToForm() {
 }
 
 
-
-fetchButton.addEventListener('click', getApiGithub);
-fetchButton.addEventListener('click', getApiLinkedIn);
+// Listening for submit button to set user information to local storage and call API
+localStorageBtn.addEventListener('click', getApiGithub);
+localStorageBtn.addEventListener('click', getApiLinkedIn);
 
 function saveToLocalStorage() {
   var firstName = document.getElementById('first-name-input').value;
   var lastName = document.getElementById('last-name-input').value;
-  var username = document.getElementById('username-input').value;
-  var password = document.getElementById('password-input-input').value;
-  var linkedInUsername = document.getElementById('LinkedIn-username').value;
   var githubUsername = document.getElementById('github-username').value;
+  var linkedInUsername = document.getElementById('LinkedIn-username').value;
 
   if (firstName === '') {
     displayMessage('error', 'First name cannot be blank.');
   } else if (lastName === '') {
     displayMessage('error', 'Last name cannot be blank.');
-  } else if (username === '') {
-    displayMessage('error', 'Username cannot be blank.');
-  } else if (password === '') {
-    displayMessage('error', 'password cannot be blank.');
   } else {
     localStorage.setItem('firstName', firstName);
     localStorage.setItem('lastName', lastName);
-    localStorage.setItem('username', username);
-    localStorage.setItem('password', password);
     localStorage.setItem('githubUsername', githubUsername);
     localStorage.setItem('linkedInUsername', linkedInUsername);
   }
